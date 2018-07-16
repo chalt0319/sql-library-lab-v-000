2abc70d1bfd4323d58351123d98e69ff705dac19
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:49:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
   JOIN characters ON characters.author_id = series.author_id
   GROUP BY characters.species
   ORDER BY COUNT(characters.species) DESC
-  LIMIT 1""
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
