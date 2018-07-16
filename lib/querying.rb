4ce0da94575fd3cdb43a1feb0cc5fcaaa320146e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:22:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-  JOIN series ON authors.id = series.author_id"
+  JOIN series ON authors.id = series.author_id
+  INNER JOIN subgenres ON series.subgenre_id = subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
