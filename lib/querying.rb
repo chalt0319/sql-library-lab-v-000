b881126598c589d5153481b8e532be720ee8910a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:56:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.author_id = series.author_id
-  GROUP BY characters.species
+  GROUP BY series.id
   ORDER BY COUNT(characters.species) DESC
   LIMIT 1"
 end
