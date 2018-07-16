f35f44d391cd04534ea387982d9772ec010d1830
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:45:54 +0000

    automatically backed up by learn

--- lib/testing.sql
+++ lib/testing.sql
@@ -5,6 +5,6 @@
 
 SELECT authors.name, subgenres.name FROM authors
 -- SELECT * FROM authors
-JOIN series ON series.author_id = authors.id;
+JOIN series ON series.author_id = authors.id
 INNER JOIN subgenres ON series.subgenre_id = subgenres.id;
 -- GROUP BY series.author_id;
