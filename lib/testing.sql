1d282f753683e441dc16538de120768757354d78
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:29:26 +0000

    automatically backed up by learn

--- lib/testing.sql
+++ lib/testing.sql
@@ -1,4 +1,9 @@
-SELECT characters.species, COUNT(characters.species) FROM characters
-GROUP BY characters.species
-ORDER BY COUNT(characters.species) DESC
-LIMIT 1;
+-- SELECT characters.species, COUNT(characters.species) FROM characters
+-- GROUP BY characters.species
+-- ORDER BY COUNT(characters.species) DESC
+-- LIMIT 1;
+
+SELECT authors.name, subgenres.name FROM authors
+JOIN series ON series.author_id = authors.id
+INNER JOIN subgenres ON series.subgenre_id = subgenre_id
+GROUP BY series.author_id;
