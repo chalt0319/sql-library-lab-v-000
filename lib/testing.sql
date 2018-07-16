cf7e4d9fb90420bcd69f8fa43bfae9cb8f96e42a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:41:42 +0000

    automatically backed up by learn

--- lib/testing.sql
+++ lib/testing.sql
@@ -3,7 +3,7 @@
 -- ORDER BY COUNT(characters.species) DESC
 -- LIMIT 1;
 
-SELECT authors.name FROM authors
+SELECT authors.name, series.author_id FROM authors
 JOIN series ON series.author_id = authors.id;
 -- INNER JOIN subgenres ON series.subgenre_id = subgenre_id
 -- GROUP BY series.author_id;
