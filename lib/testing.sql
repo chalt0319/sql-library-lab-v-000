614ce380d5da8eae71d7507fdc43fd1c5be51800
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:17:54 +0000

    automatically backed up by learn

--- lib/testing.sql
+++ lib/testing.sql
@@ -1,4 +1,4 @@
 SELECT characters.species, COUNT(characters.species) FROM characters
 GROUP BY characters.species
-ORDER BY COUNT(characters.species)
+ORDER BY COUNT(characters.species) DESC
 LIMIT 1;
