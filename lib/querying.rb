ea85ee2c2909d0556508ce79441623145c6cc868
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-16 19:26:17 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-  JOIN series ON authors.id = series.author_id
+  JOIN series ON series.author_id = authors.id
   INNER JOIN subgenres ON series.subgenre_id = subgenre_id"
 end
 
